df00e3b66fc91e395e1561f049c0a9ec761fd33f
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2021-08-05 16:21:42 -0700

    Added some starter code for CSS

---
+++ src/Board.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Tile from './Tile';
+import { TILE_COUNT, GRID_SIZE, BOARD_SIZE } from './constants';
+
+function Board() {
+    const [tiles, setTiles] = useState([...Array(TILE_COUNT).keys()]);
+
+    const pieceWidth = Math.round(BOARD_SIZE / GRID_SIZE);
+    const pieceHeight = Math.round(BOARD_SIZE / GRID_SIZE);
+    const style = {
+        width: BOARD_SIZE,
+        height: BOARD_SIZE
+    };
+
+    return (
+        <>
+            <ul style={style} className='board'>
+                {tiles.map((tile, index) => (
+                    <Tile
+                        key={tile}
+                        index={index}
+                        tile={tile}
+                        width={pieceWidth}
+                        height={pieceHeight}
+                    />
+                ))}
+            </ul>
+            <button> Start Game </button>
+        </>
+    )
+}
+
+export default Board;
